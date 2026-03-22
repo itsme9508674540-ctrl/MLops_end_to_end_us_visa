1637e9c83200ba035c4720330efa3bbbce7e61cb
itsme9508674540-ctrl <itsme9508674540@gmail.com>
2026-03-22 10:59:18 +0530

    req installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="itsme9508674540-ctrl",
+    author_email="itsme9508674540@gmail.com",
+    packages=find_packages()
+)
